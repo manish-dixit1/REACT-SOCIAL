82ec21e42125f04303a423caa0c25c2df16d2c6e
manish-dixit1 <manish.singh0617@gmail.com>
2023-04-26 21:39:03 +0530

    Create Register.jsx

---
+++ react-social/src/pages/register/Register.jsx
@@ -0,0 +1,28 @@
+import "./register.css"
+
+export default function Register() {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Manisocial</h3>
+                <span className="loginDesc">
+                    Connect with friends and people in the world around you !
+                </span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox">
+                    <input placeholder="Username" className="loginInput" />
+                    <input placeholder="Email" className="loginInput" />
+                    <input placeholder="Password" className="loginInput" />
+                    <input placeholder="Password Again" className="loginInput" />
+
+                    <button className="loginButton">Sign Up</button>
+                    
+                    <button className="loginRegister">Log into Account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
